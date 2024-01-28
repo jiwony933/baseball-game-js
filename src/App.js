e0c09bfc4d3b2397a03d6ecefb285f21d41d7e3e
Jiwon <jiwony933@naver.com>
2024-01-28 22:05:21 +0900

    feat : check score 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
+  constructor() {
+    this.randomNumbers = [];
+  }
   async play() {}
+
+  checkScore(userNumbers) {
+    const score = {
+      strike: 0,
+      ball: 0,
+    };
+    userNumbers.forEach((userNumber, index) => {
+      if (this.randomNumbers.includes(userNumber)) {
+        if (this.randomNumbers[index] === userNumber) {
+          score.strike += 1;
+        } else {
+          score.ball += 1;
+        }
+      }
+    });
+    return score;
+  }
 }
 
 export default App;
