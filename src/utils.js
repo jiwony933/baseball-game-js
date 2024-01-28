e7775f8b91cb6a6e21f79d937e443d5cc8dc4fa2
Jiwon <jiwony933@naver.com>
2024-01-28 22:12:29 +0900

    feat : print score message util 함수 구현

--- src/utils.js
+++ src/utils.js
@@ -15,3 +15,19 @@ export const makeRandomNumbers = () => {
   }
   return numbers;
 };
+
+export const printScoreMessage = (score) => {
+  const { strike, ball } = score;
+  if (strike === 0 && ball === 0) {
+    MissionUtils.Console.print("낫싱");
+  } else {
+    const message = [];
+    if (strike > 0) {
+      message.push(`${strike}스트라이크`);
+    }
+    if (ball > 0) {
+      message.push(`${ball}볼`);
+    }
+    MissionUtils.Console.print(message.join(" "));
+  }
+};
