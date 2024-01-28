3534fe9cca927969d416b2b6cfeb7f4d7ac8215c
Jiwon <jiwony933@naver.com>
2024-01-28 21:48:15 +0900

    feat : 랜덤 함수 작성

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,17 @@
-export const makeRandomNumber = () => {};
+import { MissionUtils } from "@woowacourse/mission-utils";
 
-export const makeRandomNumbers = () => {};
+export const makeRandomNumber = () => {
+  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  return number;
+};
+
+export const makeRandomNumbers = () => {
+  const numbers = [];
+  while (numbers.length < 3) {
+    const number = makeRandomNumber();
+    if (!numbers.includes(number)) {
+      numbers.push(number);
+    }
+  }
+  return numbers;
+};
